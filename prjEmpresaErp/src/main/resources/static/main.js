fc8fbc1377f387c6a1746e5da19c56247e2f6eb2
GuilhermeLealDev <guilherme.leal.dev2025@gmail.com>
2025-11-24 15:54:47 -0300

    Função de salvar funcionário adicionada

--- prjEmpresaErp/src/main/resources/static/main.js
+++ prjEmpresaErp/src/main/resources/static/main.js
@@ -6,24 +6,45 @@ async function cadastrarSetores(event) {
 
 	try {
 
-		const resposta = await fetch(`${Api}/setor/salvar`,{
-			
-			method:"POST",
-			headers:{"Content-type":"application/json"},
-			body:JSON.stringify({nomeSetor:nomeSetor})
-			
+		const resposta = await fetch(`${Api}/setor/salvar`, {
+
+			method: "POST",
+			headers: { "Content-type": "application/json" },
+			body: JSON.stringify({ nomeSetor: nomeSetor })
+
 		});
-		
-		if(!resposta.ok) throw new Error(resposta.status);
-		
-		alert("CADASTRADO COM SUCESSO!")
-		
+
+		if (!resposta.ok) throw new Error(resposta.status);
+
+		alert("SETOR CADASTRADO COM SUCESSO!")
+
 		document.getElementById("cadastroForm")
-		
+
 	} catch (err) {
-		
+
 		alert("ERRO AO CADASTRAR");
 		console.error(err)
 	}
+}
+async function cadastrarFuncionario() {
+	const nomeFuncionario = document.getElementById("nomeFuncionario").value.trim();
+	const salario = document.getElementById("salario").value.trim();
+	const setor = {
+		idSetor: document.getElementById("idSetor")
+	};
+	try {
+		const resposta = await fetch(`${Api}/funcionario/salvar`, {
+			method: "POST",
+			headers: { "Content-type": "application/json" },
+			body: JSON.stringify({ nomeFuncionario, salario, idSetor})
+		});
+		if (!resposta.ok) throw new Error(resposta.status);
+		alert("FUNCIONARIO CADASTRADO COM SUCESSO!");
+		document.getElementById("cadastroForm").reset();
+	} catch (err) {
+		alert("ERRO AO CADASTRAR");
+		console.error(err);
+	}
 
 }
+
